02627acdb6224defd16cf2fb2fc9210dc5904841
Eve <evenez@urbanarts.org>
2022-02-20 19:21:28 -0500

    Fixed Parallax
    Fixed looping objects and added a sun

--- Assets/Scripts/ParalaxController.cs
+++ Assets/Scripts/ParalaxController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ParalaxController : MonoBehaviour
 {
-    public float spd, timer; //speed of each object
+    public float spd, vspd, timer; //speed of each object
 
     public Vector3 startpos, endpos;
     //startpos - the starting position of each GameObject
@@ -23,15 +23,15 @@ public class ParalaxController : MonoBehaviour
     {
         //  timer += Time.deltaTime;
 
-        transform.position += new Vector3(spd, 0, 0);
+        //transform.position += new Vector3(spd, 0, 0);
         if (isLoop)
         {
-            transform.position += new Vector3(spd, 0, 0);
+            transform.position += new Vector3(spd, vspd, 0);
             LoopingFunction();
         }
-        else if (!isLoop && transform.position.x > endpos.x)
+        else if (!isLoop && transform.position.x > endpos.x && transform.position.y <= endpos.y)
         {
-            transform.position += new Vector3(spd, 0, 0);
+            transform.position += new Vector3(spd, vspd, 0);
         }
     }
 
