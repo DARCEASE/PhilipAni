d3eedf2fdc220b946b58a9e9eedeb45f6f49c7d2
wyntajc <wyntajc@gmail.com>
2022-02-23 22:52:28 -0500

    Added endscreen and game timer.

--- Assets/TitleScreenScript.cs
+++ Assets/TitleScreenScript.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
+using TMPro;
 public class TitleScreenScript : MonoBehaviour
 {
     public int mode;
     //0 - Title Screen
     //1 - Play Button is pressed and the transition begins
     //2 - Transition has ended
+    //3 - End Screen
     public Vector3 cloudstart, cloudend, citystart, cityend;
     public Vector2 menustart, menuend;
     public GameObject clouds, city, menu, pstore;
@@ -18,6 +20,10 @@ public class TitleScreenScript : MonoBehaviour
     public Vector3 offset;
 
     public GameObject spawner, player;
+
+    public float gameTimer;
+    public GameObject endScreen;
+    public TextMeshProUGUI counter;
     // Start is called before the first frame update
     void Start()
     {
@@ -43,6 +49,7 @@ public class TitleScreenScript : MonoBehaviour
 
         if(mode == 2)
         {
+            gameTimer += Time.deltaTime;
             player.SetActive(true);
             spawner.SetActive(true);
         }
@@ -52,6 +59,13 @@ public class TitleScreenScript : MonoBehaviour
             spawner.SetActive(false);
         }
 
+        if(gameTimer > 85)
+        {
+            
+            endScreen.SetActive(true);
+            counter.text = player.GetComponent<PlayerScript>().collidedCarAmount.ToString();
+            mode = 3;
+        }
    
     }
 
@@ -62,5 +76,10 @@ public class TitleScreenScript : MonoBehaviour
     public void QuitButton()
     {
 
+    }
+
+    public void StartOver()
+    {
+        SceneManager.LoadScene(1);
     }
 }
